0605271728024bcc2f732fcf6b86741802cbcf5d
Luc Moetwil <89918194+moetwil@users.noreply.github.com>
2024-09-23 09:26:09 +0200

    init
    springboot starter code

---
+++ src/test/kotlin/com/hearteye/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hearteye.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
