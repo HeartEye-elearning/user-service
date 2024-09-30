7e4b6f607b60a9d87da4c905b3b5094cb259de91
Luc Moetwil <89918194+moetwil@users.noreply.github.com>
2024-09-30 16:49:08 +0200

    add logger.error to the API error exception handler

--- src/main/kotlin/com/hearteye/userservice/exceptions/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/hearteye/userservice/exceptions/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.hearteye.userservice.exceptions
 
 import com.hearteye.userservice.enums.ErrorCode
+import com.hearteye.userservice.utils.getLogger
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalExceptionHandler {
 
+    private val logger = getLogger()
+
     @ExceptionHandler(APIException::class)
     fun handleApiException(
         ex: APIException,
         response: HttpServletResponse,
     ): ResponseEntity<ServiceError> {
+        logger.error("API exception: ${ex.errorCode.name} - ${ex.message}")
         return createResponse(
             status = HttpStatus.valueOf(ex.errorCode.status),
             message = ex.message,
